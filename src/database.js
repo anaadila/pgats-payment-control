fb57099b7d62f5fb196940cab5a691f79bb4ead9
Ana Ádila <anaadilacc@gmail.com>
2026-09-09 18:25:28 -0300

    chore(db): adiciona massa de teste no banco de dados local

--- src/database.js
+++ src/database.js
@@ -7,8 +7,22 @@ module.exports = {
     senha: '$2a$10$xtqV3Pqp1ZK.lrKv3i5Piee/mk4ajBGh0MPV/NR7m9GILQErHw5gW',
     nome: 'ADMIN',
     ativo: true
+  },
+  {
+    id: '00000000-0000-4000-8000-000000000002',
+    email: 'inativo@inativo.com',
+    senha: '$2a$10$xtqV3Pqp1ZK.lrKv3i5Piee/mk4ajBGh0MPV/NR7m9GILQErHw5gW',
+    nome: 'Inativo',
+    ativo: false
   }],
-  funcionarios: [],
+  funcionarios: [{
+    id: "00000000-0000-4000-8000-000000000003",
+    cpf: "123425167",
+    nome: "Joao",
+    salario_base: 10000,
+    admissao: "2028-08-08",
+    desligamento: null
+  }],
   processamentos: [],
   historicosFuncionarios: []
 };
